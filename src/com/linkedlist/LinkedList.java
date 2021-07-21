fe6ef6aa947023317b5fcd20d97c04666e668c5c
Vaishnavi9 <vaishnavidhanwade.vd@gmail.com>
2021-07-21 10:25:20 +0530

    Linkedlist

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -7,7 +7,11 @@ public class LinkedList {
         head.next = new Node(20);
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
-        print(head);
+       // print(head);
+       // head=insertBegin(89,head);
+        //printRecursive(head);
+        insertEnd(45,head);
+        printRecursive(head);
     }
 
     public static void print(Node head) {
@@ -16,6 +20,36 @@ public class LinkedList {
             System.out.print(current.data+" ");
             current=current.next;
         }
+    }
+
+
+    public static void printRecursive(Node head){
+        //Node current = head;
+        if(head==null){
+           return;
+        }
+        System.out.println(head.data);
+        head=head.next;
+        printRecursive(head);
+    }
+
+    public static Node insertBegin(int x, Node head){
+        Node temp = new Node(x);
+        temp.next=head;
+        return temp;
+    }
+
+    public static Node insertEnd(int x, Node head){
+        Node temp=new Node(x);
+        if(head==null){
+            return temp;
+        }
+        while(head.next!=null){
+            //System.out.println(head.data);
+            head=head.next;
+        }
+        head.next=temp;
+        return temp;
     }
 }
 
