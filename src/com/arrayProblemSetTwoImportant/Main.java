95b139a4ecdf925c360845ee3952a9a7be06ec9c
Vaishnavi9 <vaishnavidhanwade.vd@gmail.com>
2021-10-03 00:03:54 +0530

    array problems more

--- src/com/arrayProblemSetTwoImportant/Main.java
+++ src/com/arrayProblemSetTwoImportant/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         //subsequenceAG("GBA");
+        //377, 448, 173, 307, 108
         ArrayList<Integer> input1 = new ArrayList<Integer>(){{
             add(12);
             add(1);
@@ -17,7 +18,7 @@ public class Main {
             add(12);
         }};
 
-        minMaxSubArray(input1);
+        System.out.println(minMaxSubArraySolution1(input1));
     }
 
 
@@ -59,16 +60,74 @@ public class Main {
     */
 
     public static int minMaxSubArray(ArrayList<Integer> A) {
+        if ( A.size() == 1) return 1;
         int min = A.get(0);
         int max = A.get(0);
+        int distance = -1;
+
+        boolean foundMax = false;
+        boolean foundMin = false;
+        int maxIndex = -1;
+        int minIndex = -1;
         for ( int i=1; i < A.size(); i++) {
             if ( A.get(i) > max) max = A.get(i);
             if ( A.get(i) < min) min = A.get(i);
         }
 
-        System.out.println(min+" "+max);
+        for ( int i = 0; i < A.size(); i++ ) {
+            if ( foundMax && A.get(i) == min) {
+                minIndex = i;
+                if (maxIndex != -1) {
+                    int tempIndex = Math.abs(maxIndex - minIndex);
+                    if ( distance == -1 ) distance = tempIndex;
+                    else if ( distance > -1 && tempIndex < distance) distance = tempIndex;
+                }
+                foundMax = false;
+            }
+            else if ( foundMin && A.get(i) == max ) {
+                maxIndex = i;
+                if (minIndex != -1) {
+                    int tempIndex = Math.abs(maxIndex - minIndex);
+                    if ( distance == -1 ) distance = tempIndex;
+                    else if ( distance > -1 && tempIndex < distance) distance = tempIndex;
+                }
 
-        return 0;
+                foundMin = false;
+            }
+            else {
+                if ( A.get(i) == max ) maxIndex = i; foundMax = true;
+                if ( A.get(i) == min ) minIndex = i; foundMin = true;
+            }
+        }
+
+
+        return distance+1;
+    }
+    public static int minMaxSubArraySolution1(ArrayList<Integer> A) {
+        int min_Index = -1, max_Index = -1;
+        int min_ele = Integer.MAX_VALUE, max_ele = Integer.MIN_VALUE;
+
+        int ans = Integer.MAX_VALUE;
+        for(int x:A)
+        {
+            min_ele = Math.min(min_ele, x);
+            max_ele = Math.max(max_ele, x);
+        }
+
+        for(int i=0 ; i<A.size() ; i++)
+        {
+            if(A.get(i) == min_ele) min_Index = Math.max(min_Index, i);
+            if(A.get(i) == max_ele) max_Index = Math.max(max_Index, i);
+
+            if(min_Index != -1 && max_Index != -1)
+            {
+                int len = Math.abs(max_Index - min_Index) + 1;
+                ans = Math.min(ans, len);
+            }
+
+        }
+
+        return ans;
     }
 
     /* You are given a n x n 2D matrix A representing an image.
